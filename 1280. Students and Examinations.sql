af19215f0008c9096ce221b190fca0d31dc78f5b
Dinil Joshi <91191512+Diniljoshi@users.noreply.github.com>
2024-03-31 18:58:53 +0545

    Create 1280. Students and Examinations.sql

---
+++ 1280. Students and Examinations.sql
@@ -0,0 +1,11 @@
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+-- The result format is in the following example.
+
+select s.student_id, s.student_name, sb.subject_name, count(e.subject_name) as attended_exams
+from Students s
+cross join Subjects sb
+left join examinations e
+on sb.subject_name = e.subject_name and s.student_id = e.student_id
+group by 1,3
+order by 1,2;
