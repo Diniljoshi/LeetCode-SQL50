c6b81ddfbeb164fd78516e9485bea71176783af1
Dinil Joshi <91191512+Diniljoshi@users.noreply.github.com>
2024-04-01 16:21:17 +0545

    Update 1251. Average Selling Price.sql

--- 1251. Average Selling Price.sql
+++ 1251. Average Selling Price.sql
@@ -2,7 +2,7 @@
 -- Return the result table in any order.
 -- The result format is in the following exa
 
-select p.product_id ,round(sum(u.units*p.price)/sum(u.units),2) as average_price
+select p.product_id ,ifnull(round(sum(u.units*p.price)/sum(u.units),2),0) as average_price
 from prices p
 left join UnitsSold u
 on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
