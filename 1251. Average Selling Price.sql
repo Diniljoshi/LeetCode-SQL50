3beeca68fa613e194c4fe674bad107000c3eed03
Dinil Joshi <91191512+Diniljoshi@users.noreply.github.com>
2024-04-01 16:19:04 +0545

    Create 1251. Average Selling Price.sql

---
+++ 1251. Average Selling Price.sql
@@ -0,0 +1,9 @@
+-- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places.
+-- Return the result table in any order.
+-- The result format is in the following exa
+
+select p.product_id ,round(sum(u.units*p.price)/sum(u.units),2) as average_price
+from prices p
+left join UnitsSold u
+on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
+group by 1;
