435c25f48f34a7113d2d59f8a42b94ba059d42e9
Dinil Joshi <91191512+Diniljoshi@users.noreply.github.com>
2024-04-03 17:27:55 +0545

    Create 1045. Customers Who Bought All Products.sql

---
+++ 1045. Customers Who Bought All Products.sql
@@ -0,0 +1,10 @@
+-- Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+select customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = 
+(
+    select count(product_key)
+    from Product
+)
